fa96a6ee53b5a83b170d85e708de77e6382c5241
y3kking <99941927+y3kking@users.noreply.github.com>
2025-05-14 21:56:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,24 +11,24 @@ username=$(whoami)
 random_port=$((RANDOM % 30001 + 10000))  
 
 
-echo "to /home/$username/domains/$domain/public_html/index.js"
-curl -s -o "/home/$username/domains/$domain/public_html/index.js" "https://raw.githubusercontent.com/y3kking/whm/refs/heads/main/index.js"
+echo "to /home/$username/domains/$domain/public_html/samcod.js"
+curl -s -o "/home/$username/domains/$domain/public_html/samcod.js" "https://raw.githubusercontent.com/y3kking/whm/refs/heads/main/samcod.js"
 if [ $? -ne 0 ]; then
     echo "Error: 下载脚本 index.js 失败！"
     exit 1
 fi
 
-curl -s -o "/home/$username/cron.sh" "https://raw.githubusercontent.com/y3kking/whm/refs/heads/main/cron.sh"
+curl -s -o "/home/$username/beed.sh" "https://raw.githubusercontent.com/y3kking/whm/refs/heads/main/beed.sh"
 if [ $? -ne 0 ]; then
-    echo "Error: 下载脚本 cron.sh 失败！"
+    echo "Error: 下载脚本 sh 失败！"
     exit 1
 fi
 chmod +x /home/$username/cron.sh
 
 
 
-sed -i "s/1234.abc.com/$domain/g" "/home/$username/domains/$domain/public_html/index.js"
-sed -i "s/3000;/$random_port;/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/1234.abc.com/$domain/g" "/home/$username/domains/$domain/public_html/samcod.js"
+sed -i "s/3000;/$random_port;/g" "/home/$username/domains/$domain/public_html/samcod.js"
 
 
 cat > "/home/$username/domains/$domain/public_html/package.json" << EOF
@@ -55,7 +55,7 @@ cat > "/home/$username/domains/$domain/public_html/package.json" << EOF
 }
 EOF
 
-echo "*/5 * * * * /home/$username/cron.sh >/dev/null 2>&1" > ./mycron
+echo "*/5 * * * * /home/$username/beed.sh >/dev/null 2>&1" > ./mycron
 crontab ./mycron >/dev/null 2>&1
 rm ./mycron
 
